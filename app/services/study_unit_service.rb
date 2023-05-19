a7fc33614281fefdeb76c03bcc3c0271c672f439
eldiestrogato <andy.parkour.2119@gmail.com>
2023-05-19 16:26:47 +0200

    Comments added

--- app/services/study_unit_service.rb
+++ app/services/study_unit_service.rb
@@ -5,15 +5,20 @@ class StudyUnitService
     @parameters = parameters
   end
 
+  # After getting New Learning Path - check if Talent needs first course of Learning Path or he already has it
   def get_start_course
     if has_first_course?
+    # If the first course of Learning Path is already exists - give the next course, but only if the first is completed
       check_course(parameters[:learning_path_id])
     else
+    # Give the first course
       new_study_unit = talent_courses.build(course_id: @course)
       new_study_unit.save
     end
   end
 
+  # give the next course, but only if other is completed
+  # (On each Learning Path if the Course is belongs to several "LP-s")
   def next_course
     talent.study_lps.each do |talent_lp|
       check_course(talent_lp.learning_path_id)
@@ -22,6 +27,7 @@ class StudyUnitService
 
   private
 
+  # check if Talent needs next course of Learning Path or he has uncompleted on it
   def check_course(lp_id)
     lp_courses = fetch_lp_courses(lp_id)
     talent_courses_of_lp = talent_courses.where(course_id: lp_courses.pluck(:id))
@@ -32,6 +38,7 @@ class StudyUnitService
     end
   end
 
+  # Give next course according to sequence in Learning Path
   def get_course(lp_courses)
     lp_courses_left = lp_courses.where.not(id: talent_courses.pluck(:course_id))
     if lp_courses_left.empty?
@@ -42,18 +49,22 @@ class StudyUnitService
     end
   end
 
+  # Get collection of Courses in current Learning Path ordered by course_number
   def fetch_lp_courses(lp_id)
     LearningPath.find(lp_id).courses.order(:course_number)
   end
 
+  # Set current talent
   def talent
     Talent.find(parameters[:talent_id])
   end
 
+  # Get all courses of current Talent
   def talent_courses
     talent.study_units
   end
 
+  # Checking if Talent needs the first course of Learning Path
   def has_first_course?
     talent_courses_ids = talent.study_units.pluck(:course_id)
     @course = fetch_lp_courses(parameters[:learning_path_id]).first.id
